f4b3fd5fdca8a649fd728c17b969fa1c390060d1
Elliot Briant <briantmoliveira@gmail.com>
2018-05-01 20:14:18 -0700

    third section structure

--- marconi-app/src/components/Section/third.js
+++ marconi-app/src/components/Section/third.js
@@ -6,26 +6,60 @@ class Section3 extends Component {
     return (
       <div className="Section3">
         <div classNameName="wrapper1">
-            <div className="text-center landing-section">
+            <div id="sec" className="text-center landing-section">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 offset-md-2">
                             <h2 className="core1">Use Case</h2>
                             <br />
                         </div>
-                        <div class="row">
-                          <div class="col-md-4">
-                            <div class="card-block">
-                							<h4 class="card-title">Network Admin Apps</h4>
-                							<h6 class="card-category">Create administrative applications for network load balancing, changing network topology and IOT device management.</h6>
-                            </div>
-                          </div>
-                        </div>
                     </div>
                   </div>
               </div>
-          </div>
-      </div>
+              <div className="cardwrap">
+                <div className="row">
+          				<div className="col-md-3">
+          					<div className="card card-profile card-plain">
+          						<div className="card-block">
+                        <h4 className="card-title" style={{fontSize: '20px', fontWeight: '400', fontFamily: 'Raleway'}}>Network Admin Apps</h4>
+                        <div className="icon">
+                          <h6 className="card-category"><i className="nc-icon nc-simple-add"></i> Create administrative applications for network load balancing, changing network topology and IOT device management.</h6>
+                        </div>
+          						</div>
+          					</div>
+          				</div>
+
+
+                <div className="row">
+                  <div className="col-md-3">
+                  <div className="card card-profile card-plain">
+                    <div className="card-block">
+                      <h4 className="card-title" style={{fontSize: '20px', fontWeight: '400', fontFamily: 'Raleway'}}>Virtualized Blockchain</h4>
+                      <div className="icon">
+                        <h6 className="card-category"><i className="nc-icon nc-simple-add"></i> Jump start new blockchains that leverage the Marconi Network for secure block creation and persistence.</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className="row">
+                  <div className="col-md-3">
+                  <div className="card card-profile card-plain">
+                    <div className="card-block">
+                      <h4 className="card-title" style={{fontSize: '20px', fontWeight: '400', fontFamily: 'Raleway'}}>Security Apps</h4>
+                      <div className="icon">
+                        <h6 className="card-category"><i className="nc-icon nc-simple-add"></i> Create decentralized security apps like Anti-Phishing, Anti-Malware, Intrusion Prevention Systems and dVPNs.</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    </div>
     );
   }
 }
